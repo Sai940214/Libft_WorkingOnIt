9637f1757afc7a404b2e329fb2065a56356313d7
Yingcai Zhao <yingcai.zhao@outlook.com>
2024-04-21 15:57:21 +0300

    test done

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,5 +14,35 @@
 
  void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
+    char* x;
+    const char* y;
+    size_t  i;
+
+    x = dst;
+    y = src;
+    i = 0;
+    while (i < n)
+    {
+        x[i] = y[i];
+        i++;
+    }
+
+    return dst;
+}  
+
+#include <stdio.h>
+int main() {
+    char src[50] = "This is the source.";
+    char dest[50];
     
+    ft_memcpy(dest, src, strlen(src) + 1); 
+
+    char src2[50] = "This is eht source.";
+    char dest2[50];
+    memcpy(dest2, src2, strlen(src2) + 1);  
+
+    printf("dest = %s\n", dest); 
+    printf("dest2 = %s\n", dest2); 
+
+    return 0;
 }
